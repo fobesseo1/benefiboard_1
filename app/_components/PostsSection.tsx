c54ce2de2e0134db4db532010aec48508ef991bf
Fobes <hjdh59@gmail.com>
2024-07-21 06:41:19 +0900

    fast mainpage 22

--- app/_components/PostsSection.tsx
+++ app/_components/PostsSection.tsx
@@ -1,10 +1,9 @@
 // app/_components/PostsSection.tsx
-
 'use client';
 
 import { useState, useEffect, use } from 'react';
 import { CurrentUserType, PostType } from '@/types/types';
-import TopPosts from './TopPosts';
+import PagedPosts from '../post/_component/PagedPosts';
 
 interface PostsSectionProps {
   postsPromise: Promise<PostType[]>;
@@ -16,15 +15,18 @@ export default function PostsSection({ postsPromise, currentUser }: PostsSection
   const [posts, setPosts] = useState<PostType[]>(initialPosts);
 
   useEffect(() => {
-    // 여기서 필요하다면 추가적인 로직을 구현할 수 있습니다.
-    // 예를 들어, 로컬 스토리지를 사용한 캐싱 등
     setPosts(initialPosts);
   }, [initialPosts]);
 
   return (
     <div className="w-full flex flex-col justify-center items-center lg:w-[948px] lg:mt-4">
       <h2 className="text-center text-xl font-semibold lg:my-4 my-2">이번 주 인기 게시물</h2>
-      <TopPosts posts={posts} currentUser={currentUser} />
+      <PagedPosts
+        initialPosts={posts}
+        currentUser={currentUser}
+        userId={currentUser?.id ?? null}
+        isTopPosts={true}
+      />
     </div>
   );
 }
--- app/post/_component/PagedPosts.tsx
+++ app/post/_component/PagedPosts.tsx
@@ -6,101 +6,124 @@ import Link from 'next/link';
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 import { SlBubble, SlEye, SlHeart } from 'react-icons/sl';
 import { listformatDate } from '@/lib/utils/formDate';
-import { fetchMorePosts, fetchSearchPosts } from '../_action/infinityScrollPost';
-import { CurrentUserType, PostType } from '../../../types/types';
 import { addWritingPoints, addDonationPoints } from '../_action/adPointSupabase';
+import { CurrentUserType, PostType } from '../../../types/types';
 import { Button } from '@/components/ui/button';
 
 type PagedPostsProps = {
   initialPosts: PostType[];
   userId?: string | null;
-  searchTerm?: string;
-  categoryId?: string;
   currentUser: CurrentUserType | null;
-  totalCount: number;
-  currentPage: number;
+  totalCount?: number;
+  currentPage?: number;
+  isTopPosts?: boolean;
+  categoryId?: string;
   isBestPosts?: boolean;
+  searchTerm?: string;
 };
 
 const PostItem = React.memo(({ post, isRead }: { post: PostType; isRead: boolean }) => (
-  <Link href={`/post/detail/${post.id}`} passHref prefetch={true}>
+  <div className="">
+    {/* Mobile view */}
     <div className="flex flex-col py-2 bg-white border-b-[1px] border-gray-200 lg:hidden">
-      {/* Mobile layout */}
       <div className="flex justify-between items-center">
-        <div className="categoryCreatorComments flex gap-2 flex-1 overflow-hidden items-center">
-          <p className="text-xs leading-tight tracking-tight text-gray-600">
-            {post.parent_category_name || '아무거나'} &gt; {post.child_category_name || '프리토크'}
-          </p>
+        <div className="categoryCreatorComments flex gap-2 flex-1 overflow-hidden">
+          <div className="flex">
+            <p className="text-xs leading-tight tracking-tight text-gray-600">
+              {post.parent_category_name || '아무거나'} &gt;
+            </p>
+            <p className="text-xs leading-tight tracking-tight text-gray-600 ml-1">
+              {post.child_category_name || '프리토크'}
+            </p>
+          </div>
         </div>
         <p className="text-xs text-gray-600">{listformatDate(post.created_at) || 'No time'}</p>
       </div>
-      <div className="flex-1 pt-2 pb-2">
+      <div className="flex-1 pt-2 pb-2 cursor-pointer">
         <p
-          className={`font-semibold line-clamp-1 leading-tight tracking-tighter ${isRead ? 'text-gray-400' : ''}`}
+          className={`font-semibold line-clamp-1 leading-tight tracking-tighter ${
+            isRead ? 'text-gray-400' : ''
+          }`}
         >
           {post.title}
         </p>
       </div>
-      {/* Post metadata */}
       <div className="flex gap-4 items-center overflow-hidden">
-        <p
-          className={`text-xs font-semibold leading-tight tracking-tight truncate ${isRead ? 'text-gray-400' : ''}`}
-        >
-          {post.author_name || post.author_email || 'unknown'}
-        </p>
+        <div className="overflow-hidden flex-1">
+          <p className="text-xs font-semibold leading-tight tracking-tight text-gray-600 truncate">
+            {post.author_name || post.author_email || 'unknown'}
+          </p>
+        </div>
         <div className="flex gap-1">
-          <span className="flex items-center gap-[2px]">
+          <div className="flex items-center gap-[2px]">
             <SlHeart size={12} color="gray" />
-            {post.likes || '0'}
-          </span>
-          <span className="flex items-center gap-[2px]">
+            <p className="text-xs leading-tight tracking-tight text-gray-600">
+              {post.likes || '0'}
+            </p>
+          </div>
+          <div className="flex items-center gap-[2px]">
             <SlEye size={14} color="gray" />
-            {post.views || '0'}
-          </span>
-          <span className="flex items-center gap-[2px]">
+            <p className="text-xs leading-tight tracking-tight text-gray-600">
+              {post.views || '0'}
+            </p>
+          </div>
+          <div className="flex items-center gap-[2px]">
             <SlBubble size={12} color="gray" />
-            {post.comments || '0'}
-          </span>
+            <p className="text-xs leading-tight tracking-tight text-gray-600">
+              {post.comments || '0'}
+            </p>
+          </div>
         </div>
       </div>
     </div>
 
-    <div className="hidden lg:flex w-[948px] mx-auto gap-4 items-center justify-between py-2 bg-white border-b-[1px] border-gray-200">
-      {/* Desktop layout */}
-      <div className="w-[160px]">
-        <p className="text-xs leading-tight tracking-tight text-gray-600">
-          {post.parent_category_name || '아무거나'} &gt; {post.child_category_name || '프리토크'}
-        </p>
+    {/* Desktop view */}
+    <div className="hidden lg:flex w-[948px] gap-4 items-center py-2 bg-white border-b-[1px] border-gray-200">
+      <div className="flex items-center w-[160px]">
+        <div className="flex">
+          <p className="text-xs leading-tight tracking-tight text-gray-600">
+            {post.parent_category_name || '아무거나'} &gt;
+          </p>
+          <p className="text-xs leading-tight tracking-tight text-gray-600 ml-1">
+            {post.child_category_name || '프리토크'}
+          </p>
+        </div>
       </div>
-      <div className="w-[520px] py-1">
+      <div className="w-[520px] py-1 cursor-pointer">
         <p
-          className={`font-semibold line-clamp-1 leading-tight tracking-tighter ${isRead ? 'text-gray-400' : ''}`}
+          className={`font-semibold line-clamp-1 leading-tight tracking-tighter ${
+            isRead ? 'text-gray-400' : ''
+          }`}
         >
           {post.title}
         </p>
       </div>
-      <p className="w-[120px] text-sm leading-tight tracking-tight text-gray-600 truncate">
-        {post.author_name || post.author_email || 'unknown'}
-      </p>
-      <div className="flex gap-1 w-[100px]">
-        <span className="flex items-center gap-[2px]">
+      <div className="overflow-hidden w-[100px]">
+        <p className="text-sm leading-tight tracking-tight text-gray-600 truncate">
+          {post.author_name || post.author_email || 'unknown'}
+        </p>
+      </div>
+      <div className="flex gap-1 w-[120px]">
+        <div className="flex items-center gap-[2px]">
           <SlHeart size={12} color="gray" />
-          {post.likes || '0'}
-        </span>
-        <span className="flex items-center gap-[2px]">
+          <p className="text-xs leading-tight tracking-tight text-gray-600">{post.likes || '0'}</p>
+        </div>
+        <div className="flex items-center gap-[2px]">
           <SlEye size={14} color="gray" />
-          {post.views || '0'}
-        </span>
-        <span className="flex items-center gap-[2px]">
+          <p className="text-xs leading-tight tracking-tight text-gray-600">{post.views || '0'}</p>
+        </div>
+        <div className="flex items-center gap-[2px]">
           <SlBubble size={12} color="gray" />
-          {post.comments || '0'}
-        </span>
+          <p className="text-xs leading-tight tracking-tight text-gray-600">
+            {post.comments || '0'}
+          </p>
+        </div>
       </div>
       <p className="text-xs text-gray-600 lg:block w-[48px]">
         {listformatDate(post.created_at) || 'No time'}
       </p>
     </div>
-  </Link>
+  </div>
 ));
 
 PostItem.displayName = 'PostItem';
@@ -108,20 +131,21 @@ PostItem.displayName = 'PostItem';
 export default function PagedPosts({
   initialPosts,
   userId,
-  searchTerm,
-  categoryId,
   currentUser,
   totalCount,
   currentPage,
+  isTopPosts = false,
+  categoryId,
   isBestPosts = false,
+  searchTerm,
 }: PagedPostsProps) {
-  const [posts] = useState<PostType[]>(initialPosts);
+  const [posts, setPosts] = useState<PostType[]>(initialPosts);
   const [readPosts, setReadPosts] = useState<Record<string, boolean>>({});
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const totalPages = useMemo(() => Math.ceil(totalCount / 20), [totalCount]);
+  const totalPages = useMemo(() => (totalCount ? Math.ceil(totalCount / 20) : 1), [totalCount]);
 
   useEffect(() => {
     if (userId) {
@@ -136,66 +160,80 @@ export default function PagedPosts({
     }
   }, [userId]);
 
-  useEffect(() => {
-    const postId = pathname.split('/').pop();
-    if (postId && !readPosts[postId]) {
-      setReadPosts((prev) => ({ ...prev, [postId]: true }));
+  const handlePostClick = useCallback(
+    (post: PostType) => {
+      setReadPosts((prev) => ({ ...prev, [post.id]: true }));
+
       if (userId) {
         const readPostsKey = `readPosts_${userId}`;
         localStorage.setItem(
           readPostsKey,
-          JSON.stringify(Object.keys({ ...readPosts, [postId]: true }))
+          JSON.stringify(Object.keys({ ...readPosts, [post.id]: true }))
         );
       }
 
-      const post = posts.find((p) => p.id === postId);
-      if (post) {
-        Promise.all([
-          addWritingPoints(post.author_id, 5),
-          currentUser?.donation_id
-            ? addDonationPoints(currentUser.id, currentUser.donation_id, 5)
-            : Promise.resolve(),
-        ]).catch((error) => {
-          console.error('Error adding points:', error);
-        });
-      }
-    }
-  }, [pathname, posts, readPosts, userId, currentUser]);
+      Promise.all([
+        addWritingPoints(post.author_id, 5),
+        currentUser?.donation_id
+          ? addDonationPoints(currentUser.id, currentUser.donation_id, 5)
+          : Promise.resolve(),
+      ]).catch((error) => {
+        console.error('Error adding points:', error);
+      });
+
+      router.push(`/post/detail/${post.id}`);
+    },
+    [readPosts, userId, currentUser, router]
+  );
 
   const handlePageChange = useCallback(
     (newPage: number) => {
-      const current = new URLSearchParams(Array.from(searchParams.entries()));
-      current.set('page', newPage.toString());
-      const search = current.toString();
-      const query = search ? `?${search}` : '';
-      router.push(`${pathname}${query}`);
+      if (!isTopPosts && currentPage !== undefined) {
+        const current = new URLSearchParams(Array.from(searchParams.entries()));
+        current.set('page', newPage.toString());
+        const search = current.toString();
+        const query = search ? `?${search}` : '';
+        router.push(`${pathname}${query}`);
+      }
     },
-    [searchParams, pathname, router]
+    [isTopPosts, currentPage, searchParams, pathname, router]
   );
 
   return (
-    <div>
+    <div className="relative lg:w-[948px] mx-auto w-full ">
       {posts.map((post) => (
-        <PostItem key={post.id} post={post} isRead={readPosts[post.id] || false} />
+        <div key={post.id} onClick={() => handlePostClick(post)}>
+          <PostItem post={post} isRead={readPosts[post.id] || false} />
+        </div>
       ))}
 
-      <div className="flex justify-between items-center mt-4 lg:w-[948px] mx-auto">
-        <Button
-          onClick={() => handlePageChange(Math.max(currentPage - 1, 1))}
-          disabled={currentPage === 1}
-        >
-          Previous Page
-        </Button>
-        <span>
-          Page {currentPage} of {totalPages}
-        </span>
-        <Button
-          onClick={() => handlePageChange(Math.min(currentPage + 1, totalPages))}
-          disabled={currentPage === totalPages}
-        >
-          Next Page
-        </Button>
-      </div>
+      {isTopPosts && (
+        <Link href="/post/best" prefetch={true}>
+          <div className="absolute w-8 h-8 -bottom-4 left-1/2 transform -translate-x-1/2 bg-red-400 flex items-center justify-center rounded-full">
+            <p className="text-lg font-semibold text-white">+</p>
+          </div>
+        </Link>
+      )}
+
+      {!isTopPosts && currentPage !== undefined && (
+        <div className="flex justify-between items-center mt-4 lg:w-[948px] mx-auto">
+          <Button
+            onClick={() => handlePageChange(Math.max(currentPage - 1, 1))}
+            disabled={currentPage === 1}
+          >
+            Previous Page
+          </Button>
+          <span>
+            Page {currentPage} of {totalPages}
+          </span>
+          <Button
+            onClick={() => handlePageChange(Math.min(currentPage + 1, totalPages))}
+            disabled={currentPage === totalPages}
+          >
+            Next Page
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
