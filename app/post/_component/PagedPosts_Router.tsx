190682256f77e1697944797943b5fe6c8ab4b800
Fobes <hjdh59@gmail.com>
2024-07-23 08:40:51 +0900

    go to post detail No transition

--- app/post/_component/PagedPosts_Router.tsx
+++ app/post/_component/PagedPosts_Router.tsx
@@ -164,8 +164,8 @@ export default function PagedPosts({
   const handlePostClick = useCallback(
     (post: PostType) => {
       // 즉시 페이지 이동 시작
-      router.push(`/experiment`);
-      /* router.push(`/post/detail/${post.id}`); */
+      /* router.push(`/experiment`); */
+      router.push(`/post/detail/${post.id}`);
 
       /* // 비중요 작업을 transition으로 감싸기
       startTransition(() => {
