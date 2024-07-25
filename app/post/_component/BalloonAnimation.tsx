65046aff69cd142d0f321521462ce16f27c607ca
Fobes <hjdh59@gmail.com>
2024-07-25 23:15:50 +0900

    풍선애니메이션 수정0-20-40

--- app/post/_component/BalloonAnimation.tsx
+++ app/post/_component/BalloonAnimation.tsx
@@ -22,11 +22,14 @@ const Balloon: React.FC<BalloonProps> = ({ delay, maxWidth, content, contentType
 
   const props = useSpring({
     from: {
-      transform: `translateX(${startX}px) translateY(20vh) scale(0.5)`,
+      transform: `translateX(${startX}px) translateY(0vh) scale(0.5)`,
       opacity: 0,
     },
     to: async (next) => {
-      await next({ opacity: 0.75, transform: `translateX(${startX}px) translateY(0vh) scale(1)` });
+      await next({
+        opacity: 0.75,
+        transform: `translateX(${startX}px) translateY(-20vh) scale(1)`,
+      });
       await next({ transform: `translateX(${endX}px) translateY(-40vh) scale(1)` });
     },
     config: { ...config.gentle, duration: 2000 }, // 애니메이션 시간을 3000ms로 줄임
